a8ae8c8594b957247e5c9276b016366079487ff5
Phoo Siang Chuen <phoosiangchuen@gmail.com>
2013-07-10 15:45:35 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -2,8 +2,11 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var fs = require('fs');
+
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  var buf = new Buffer(fs.readFile('index.html'));  
+  response.send(buf.toString());
 });
 
 var port = process.env.PORT || 5000;
